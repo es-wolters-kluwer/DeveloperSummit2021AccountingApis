9ea5c0e3fc856b9eea1c5127c29aee84949c5b1a
sergio.cortes <sergio.cortes>
2021-11-25 13:40:28 +0100

    Accounts list

---
+++ src/WkeInnuvaDeveloperSummit/Controllers/AccountsController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace WkeInnuvaDeveloperSummit.Controllers
+{
+    public class AccountsController : WkeBaseController
+    {
+        public AccountsController(
+            IConfiguration configuration,
+            ILogger<HomeController> logger) 
+            : base(configuration)
+        {
+        }
+
+        // GET: AccountsController
+        public async Task<IActionResult> Index(string companyCorrelationId)
+        {
+
+            using (var client = this.CreateHttpClient(companyCorrelationId))
+            {
+                var accountsUrl = $"{this.wkeDeveloperPortalConfiguration.AccountingEndPoint}api/accounts?pageNumber=1&pageSize=1000";
+                var accountsRequest = await client.GetAsync(accountsUrl);
+
+                accountsRequest.EnsureSuccessStatusCode();
+
+                var result = await accountsRequest.Content.ReadAsStringAsync();
+                var accounts = JsonConvert.DeserializeObject<List<AccountModel>>(result);
+
+                ViewBag.CompanyCorrelationId = companyCorrelationId;
+
+                return View(accounts);
+            }
+
+        }
+
+    }
+}
---
+++ src/WkeInnuvaDeveloperSummit/Wke/Models/AccountModel.cs
@@ -0,0 +1,20 @@
+﻿namespace WkeInnuvaDeveloperSummit.Wke
+{
+    public class AccountModel
+    {
+        public string Id
+        {
+            get
+            {
+                return this.IsTemplate ? Guid.NewGuid().ToString() : this.CorrelationId;
+            }
+        }
+        public string CorrelationId { get; set; }
+        public string Code { get; set; }
+        public string Description { get; set; }
+        public string State { get; set; }
+        public DateTime Version { get; set; }
+        public long SequenceVersion { get; set; }
+        public bool IsTemplate => this.State == "Template";
+    }
+}
