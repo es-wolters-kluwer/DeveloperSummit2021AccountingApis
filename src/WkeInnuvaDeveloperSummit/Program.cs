21014c976cbefed4eedd21afc6b0650faf419648
sergio.cortes <sergio.cortes>
2021-11-25 13:19:30 +0100

    Wolters Kluwer Identity Authorization

--- src/WkeInnuvaDeveloperSummit/Program.cs
+++ src/WkeInnuvaDeveloperSummit/Program.cs
@@ -1,9 +1,11 @@
 global using WkeInnuvaDeveloperSummit.Wke;
-
+using IdentityModel;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +15,7 @@ builder.Services
     .AddRazorRuntimeCompilation();
 
 var woltersKluwerIdentityConfiguration = builder.Configuration.GetSection("WkeOAuthClientConfiguration").Get<WkeOAuthClientConfigurationModel>();
+var authenticationTools = new WkeAuthonticationTools();
 
 var cookieAuthenticationEvents = new CookieAuthenticationEvents()
 {
@@ -20,6 +23,50 @@ var cookieAuthenticationEvents = new CookieAuthenticationEvents()
 
 var openIdConnectEvents = new OpenIdConnectEvents()
 {
+
+    OnAuthorizationCodeReceived = async (context) =>
+    {
+
+        var result = await authenticationTools.ProcessAuthorizationCodeAsync(
+                     context,
+                     woltersKluwerIdentityConfiguration.Authority,
+                     woltersKluwerIdentityConfiguration.ClientId,
+                     woltersKluwerIdentityConfiguration.ClientSecret)
+                 .ConfigureAwait(true);
+
+        var accessToken = result.AccessTokenResponse.AccessToken;
+
+        var jsonToken = (JwtSecurityToken)new JwtSecurityTokenHandler().ReadToken(accessToken);
+        var tenantId = jsonToken.Claims.Single(claim => claim.Type == WkeConstants.ClaimTenant).Value;
+
+        result.Claims.Add(new Claim(WkeConstants.ClaimTenant, tenantId));
+        result.Claims.Add(new Claim(WkeConstants.ClaimAccessToken, accessToken));
+        result.Claims.Add(new Claim(WkeConstants.ClaimExpiresAt, DateTime.Now.AddSeconds(result.AccessTokenResponse.ExpiresIn).ToLocalTime().ToString()));
+        result.Claims.Add(new Claim(WkeConstants.ClaimRefreshToken, result.AccessTokenResponse.RefreshToken));
+        result.Claims.Add(new Claim(WkeConstants.ClaimIdToken, context.ProtocolMessage.IdToken));
+
+        jsonToken.Claims.Where(claim => claim.Type == JwtClaimTypes.Scope)
+            .ToList().ForEach(
+                claim => result.Claims.Add(new Claim(JwtClaimTypes.Scope, claim.Value))
+            );
+
+        jsonToken.Claims.Where(claim => claim.Type != JwtClaimTypes.Scope)
+            .ToList().ForEach(
+                claim =>
+                {
+                    if (!result.Claims.Contains(claim))
+                    {
+                        result.Claims.Add(claim);
+                    }
+                });
+
+        ((ClaimsIdentity)context.Principal.Identity).AddClaims(
+            result.Claims.Distinct(new ClaimComparer()));
+
+        context.Success();
+
+    }
+
 };
 
 builder.Services
