beffd76fb14bf9b2501496b3baa79da0e8c97a68
sergio.cortes <sergio.cortes>
2021-11-25 13:13:24 +0100

    Wolters Kluwer Identity Authorization

--- src/WkeInnuvaDeveloperSummit/Program.cs
+++ src/WkeInnuvaDeveloperSummit/Program.cs
@@ -1,9 +1,70 @@
+global using WkeInnuvaDeveloperSummit.Wke;
+
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services
     .AddControllersWithViews()
     .AddRazorRuntimeCompilation();
+
+var woltersKluwerIdentityConfiguration = builder.Configuration.GetSection("WkeOAuthClientConfiguration").Get<WkeOAuthClientConfigurationModel>();
+
+var cookieAuthenticationEvents = new CookieAuthenticationEvents()
+{
+};
+
+var openIdConnectEvents = new OpenIdConnectEvents()
+{
+};
+
+builder.Services
+    .AddAuthentication(
+        options =>
+        {
+            options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+        })
+    .AddCookie(
+        CookieAuthenticationDefaults.AuthenticationScheme,
+        authenticationOptions =>
+        {
+            authenticationOptions.Cookie.Name = ".Webhook";
+            authenticationOptions.ExpireTimeSpan = TimeSpan.FromDays(1);
+            authenticationOptions.AccessDeniedPath = PathString.FromUriComponent("/Error");
+            authenticationOptions.Events = cookieAuthenticationEvents;
+        })
+    .AddOpenIdConnect(
+        connectOptions =>
+        {
+            connectOptions.Authority = woltersKluwerIdentityConfiguration.Authority;
+            connectOptions.ClientId = woltersKluwerIdentityConfiguration.ClientId;
+            connectOptions.ClientSecret = woltersKluwerIdentityConfiguration.ClientSecret;
+            connectOptions.SignedOutRedirectUri = $"{woltersKluwerIdentityConfiguration.RedirectUrl}";
+            connectOptions.AuthenticationMethod = OpenIdConnectRedirectBehavior.RedirectGet;
+            connectOptions.GetClaimsFromUserInfoEndpoint = true;
+            connectOptions.RequireHttpsMetadata = false;
+            connectOptions.SaveTokens = true;
+            connectOptions.Scope.Clear();
+            connectOptions.ClaimActions.MapAll();
+            foreach (var scope in woltersKluwerIdentityConfiguration.Scopes)
+            {
+                connectOptions.Scope.Add(scope);
+            }
+
+            connectOptions.ResponseType = woltersKluwerIdentityConfiguration.ResponseType;
+            connectOptions.Events = openIdConnectEvents;
+            connectOptions.TokenValidationParameters = new TokenValidationParameters
+            {
+                NameClaimType = WkeConstants.ClaimGivenName,
+                AuthenticationType = woltersKluwerIdentityConfiguration.AuthenticationType,
+                SaveSigninToken = true
+            };
+        });
 
 var app = builder.Build();
 
@@ -20,6 +81,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
---
+++ src/WkeInnuvaDeveloperSummit/Wke/WkeConstants.cs
@@ -0,0 +1,14 @@
+﻿namespace WkeInnuvaDeveloperSummit.Wke
+{
+    public static class WkeConstants
+    {
+
+        public const string ClaimAccessToken = "access_token";
+        public const string ClaimGivenName = "given_name";
+        public const string ClaimTenant = "wk.es.idcda";
+        public const string ClaimExpiresAt = "expires_at";
+        public const string ClaimRefreshToken = "refresh_token";
+        public const string ClaimIdToken = "id_token";
+
+    }
+}
